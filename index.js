f23b689edcc6e3f6b245053c22cc2349eb2c571e
Jefe <itisrossi@gmail.com>
2020-01-23 18:48:42 +0100

    second commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 //Homepage Route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-//Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/login', function (req, res) {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.send(username);
+});
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
